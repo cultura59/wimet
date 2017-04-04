aa49291e8d22667d51444dd1db927891ea91a773
rojasa19 <rojasadrian.e@gmail.com>
2017-04-04 18:24:20 -0300

    Se arregla bug de estilos

--- resources/views/dashboard/menue.blade.php
+++ resources/views/dashboard/menue.blade.php
@@ -1,10 +1,13 @@
 <aside>
-	<ul class="list-sub-menu">
-		<li class="list-sub-menu_item--active"><a href="{{url('/dashboard/user/1')}}">Dashboard</a></li>
-		<li class="list-sub-menu_item"><a href="{{url('/dashboard/user/1/consultas')}}">Consultas</a></li>
-		<li class="list-sub-menu_item"><a href="{{url('/dashboard/user/1/reservas')}}">Reservas</a></li>
-		<li class="list-sub-menu_item"><a href="{{url('/dashboard/user/1/solicitudes')}}">Solicitudes</a></li>
-		<li class="list-sub-menu_item"><a href="{{url('/dashboard/user/1/favoritos')}}">Mis Favoritos</a></li>
-		<li class="list-sub-menu_item"><a href="{{url('/dashboard/user/1')}}">Mis Espacios</a></li>
+	<ul>
+		<li class="title">RESERVAS</li>
+		<li class="active"><a href="{{url('/dashboard/user/1/consultas')}}">Mensajes</a></a></li>
+		<li><a href="{{url('/dashboard/user/1/reservas')}}">Reservas</a></li>
+		<li><a href="{{url('/dashboard/user/1/consultas')}}">Evaluaciones</a></li>
+		<li><a href="{{url('/dashboard/user/1/favoritos')}}">Favoritos</a></li>
+		<li><a href="{{url('/dashboard/user/1/consultas')}}">Mis espacios</a></li>
+		<li class="title">DATOS PERSONALES</li>
+		<li><a href="{{url('/dashboard/user/1/consultas')}}">Mi perfil</a></li>
+		<li><a href="{{url('/dashboard/user/1/consultas')}}">Cuenta</a></li>
 	</ul>
 </aside>
