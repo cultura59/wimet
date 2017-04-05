93e5f8d1259e757f69b32309abddd7393397da7b
rojasa19 <rojasadrian.e@gmail.com>
2017-04-04 22:02:08 -0300

    Se crea la vista de mis espacios

--- resources/views/dashboard/menue.blade.php
+++ resources/views/dashboard/menue.blade.php
@@ -5,7 +5,7 @@
 		<li><a href="{{url('/dashboard/user/1/reservas')}}">Reservas</a></li>
 		<li><a href="{{url('/dashboard/user/1/consultas')}}">Evaluaciones</a></li>
 		<li><a href="{{url('/dashboard/user/1/favoritos')}}">Favoritos</a></li>
-		<li><a href="{{url('/dashboard/user/1/consultas')}}">Mis espacios</a></li>
+		<li><a href="{{url('/dashboard/user/1/misespacios')}}">Mis espacios</a></li>
 		<li class="title">DATOS PERSONALES</li>
 		<li><a href="{{url('/dashboard/user/1/consultas')}}">Mi perfil</a></li>
 		<li><a href="{{url('/dashboard/user/1/consultas')}}">Cuenta</a></li>
