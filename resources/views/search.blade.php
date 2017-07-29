ea17e93be12df47d12104d249ee38849e038a7c7
rojasa19 <rojasadrian.e@gmail.com>
2017-07-29 14:37:50 -0300

    se arregla el icono mapa search

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -58,7 +58,7 @@
 	            '</div>';
 				marker = new google.maps.Marker({
 					position: new google.maps.LatLng({{$espacio->lat}}, {{$espacio->long}}),
-					icon: 'http://wimet.co/wimet_iso.ico',
+					icon: 'http://wimet.co/img/wimet_ic_place_red_24px.ico',
 					map: map,
 					title: '{{$espacio->name}}'
 				});
