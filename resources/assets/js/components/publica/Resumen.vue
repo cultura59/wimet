8efd61e812a1d2eb6807a4536f1cee103bd4c156
Adrian <adrian@wimet.co>
2017-12-25 21:01:35 -0300

    se modifica pantalla de resumen

--- resources/assets/js/components/publica/Resumen.vue
+++ resources/assets/js/components/publica/Resumen.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="wt-m-top-3">
-        <h1 class="publica-titulo">¡Lo estás haciendo muy bien!</h1>
+        <h1 v-if="$store.getters.getEspacio.step < 6" class="publica-titulo">¡Lo estás haciendo muy bien!</h1>
+        <template v-if="$store.getters.getEspacio.step == 6">
+            <h1 class="publica-titulo">¡Felicitaciones {{$store.getters.getUser.firstname}}!</h1>
+            <p class="wt-m-top-1">Tu espacio será publicado en Wimet.co una vez aprobado por nuestro equipo.</p>
+            <button class="btn-complete" @click="aprobacion()">ENVIAR PARA APROBACIÓN</button>
+        </template>
         <div class="row">
             <div class="wt-m-top-2 col-xs-12 col-md-6">
                 <div class="box-container">
@@ -19,8 +24,13 @@
                         <span class="box-container_sub-title">Reuniones - Eventos - Producciones</span>
                     </div>
                     <div class="box-container__btn">
-                        <router-link :to="`/actividad/${$store.getters.getEspacio.prices[0].categoria.name}`" class="box-container__btn__edit">EDITAR</router-link>
-                        <img src="/img/wimet_ic_check_24px_accent.svg">
+                        <template v-if="$store.getters.getEspacio.step > 1">
+                            <router-link :to="`/actividad/${$store.getters.getEspacio.prices[0].categoria.name}`" class="box-container__btn__edit">EDITAR</router-link>
+                            <img src="/img/wimet_ic_check_24px_accent.svg">
+                        </template>
+                        <button v-if="$store.getters.getEspacio.step <= 1" class="btn-complete">
+                            <router-link :to="`/actividad/${$store.getters.getEspacio.prices[0].categoria.name}`">COMPLETAR</router-link>
+                        </button>
                     </div>
                 </div>
                 <div class="box-container">
@@ -29,8 +39,13 @@
                         <span class="box-container_sub-title">Acceso - Amenities - Equipamiento</span>
                     </div>
                     <div class="box-container__btn">
-                        <router-link to="/amenities" class="box-container__btn__edit">EDITAR</router-link>
-                        <img src="/img/wimet_ic_check_24px_accent.svg">
+                        <template v-if="$store.getters.getEspacio.step > 2">
+                            <router-link to="/amenities" class="box-container__btn__edit">EDITAR</router-link>
+                            <img src="/img/wimet_ic_check_24px_accent.svg">
+                        </template>
+                        <button v-if="$store.getters.getEspacio.step <= 2" class="btn-complete">
+                            <router-link to="/amenities">COMPLETAR</router-link>
+                        </button>
                     </div>
                 </div>
                 <div class="box-container">
@@ -39,8 +54,13 @@
                         <span class="box-container_sub-title">Días de la semana</span>
                     </div>
                     <div class="box-container__btn">
-                        <router-link to="/disponibilidad" class="box-container__btn__edit">EDITAR</router-link>
-                        <img src="/img/wimet_ic_check_24px_accent.svg">
+                        <template v-if="$store.getters.getEspacio.step > 3">
+                            <router-link to="/disponibilidad" class="box-container__btn__edit">EDITAR</router-link>
+                            <img src="/img/wimet_ic_check_24px_accent.svg">
+                        </template>
+                        <button v-if="$store.getters.getEspacio.step <= 3" class="btn-complete">
+                            <router-link to="/disponibilidad">COMPLETAR</router-link>
+                        </button>
                     </div>
                 </div>
                 <div class="box-container">
@@ -49,8 +69,13 @@
                         <span class="box-container_sub-title">Imágenes en alta calidad</span>
                     </div>
                     <div class="box-container__btn">
-                        <router-link to="/foto" class="box-container__btn__edit">EDITAR</router-link>
-                        <img src="/img/wimet_ic_check_24px_accent.svg">
+                        <template v-if="$store.getters.getEspacio.step > 4">
+                            <router-link to="/foto" class="box-container__btn__edit">EDITAR</router-link>
+                            <img src="/img/wimet_ic_check_24px_accent.svg">
+                        </template>
+                        <button v-if="$store.getters.getEspacio.step <= 4" class="btn-complete">
+                            <router-link to="/foto">COMPLETAR</router-link>
+                        </button>
                     </div>
                 </div>
                 <div class="box-container">
@@ -59,8 +84,13 @@
                         <span class="box-container_sub-title">Título - Descripción - Reglamento interno</span>
                     </div>
                     <div class="box-container__btn">
-                        <router-link to="/descripcion" class="box-container__btn__edit">EDITAR</router-link>
-                        <img src="/img/wimet_ic_check_24px_accent.svg">
+                        <template v-if="$store.getters.getEspacio.step > 5">
+                            <router-link to="/descripcion" class="box-container__btn__edit">EDITAR</router-link>
+                            <img src="/img/wimet_ic_check_24px_accent.svg">
+                        </template>
+                        <button v-if="$store.getters.getEspacio.step <= 5" class="btn-complete">
+                            <router-link to="/descripcion">COMPLETAR</router-link>
+                        </button>
                     </div>
                 </div>
             </div>
@@ -73,7 +103,29 @@
 
 <script>
     export default {
-        name: "resumen"
+        name: "resumen",
+        methods: {
+            aprobacion() {
+                this.$store.getters.getEspacio.step = 7;
+                this.$http.put(`api/espacio/${this.$store.getters.getEspacio.id}`, this.$store.getters.getEspacio)
+                .then(res => {
+                    this.getEspacio(this.$store.getters.getEspacio.id);
+                    this.$toastr.success("El espacio fue enviado para aprobar", 'Exito!!!');
+                }, err => {
+                    this.$toastr.error(err, 'Ups hubo un error');
+                });
+            },
+            getEspacio(id) {
+                this.$http.get(`api/espacio/${id}`)
+                .then(res => {
+                    this.$store.commit('setEspacio', res.body);
+                    this.$router.push({ name: "resumen"});
+                }, err => {
+                    this.btnSend = true;
+                    $toastr.error("Ups...", "Hubo un problema al modificar su espacio, vuelva a intentarlo");
+                });
+            }
+        }
     }
 </script>
 
@@ -104,6 +156,17 @@
             img {
                 height: 23px;
             }
+        }
+    }
+    .btn-complete {
+        padding: 1em;
+        border: none;
+        background-color: #fc5289;
+        font-size: 12px;
+        color: #ffffff;
+        a {
+            color: #FFFFFF;
+            text-decoration: none;
         }
     }
 </style>
