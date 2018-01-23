31ee46a48a49761fae1d62dc68d0f822649cb719
Adrian <adrian@wimet.co>
2018-01-23 13:38:25 -0300

    Se agrega msj in_process

--- app/Http/Controllers/MercadoPagoController.php
+++ app/Http/Controllers/MercadoPagoController.php
@@ -75,6 +75,22 @@ class MercadoPagoController extends Controller
                         ->queue(new ConsultaAnfitrion($duenio, $request['espacio'], $user));
 
                     return $user;
+                case 'in_process':
+                    $senia = new UserSenias();
+                    $senia->user_id = $request['user_id'];
+                    $senia->paymentid = $res['response']['id'];
+                    $senia->vencimiento = $request['vencimiento'];
+                    $senia->save();
+                    $user = User::with('senias')->where('id', $request['user_id'])->first();
+                    $duenio = User::find($request['espacio']["user_id"]);
+
+                    $emails = ['federico@wimet.co', 'alejandro@wimet.co','adrian@wimet.co'];
+                    // Email al organizador (Datos del espacio)
+                    Mail::to($user->email)
+                        ->bcc($emails)
+                        ->queue(new ConsultaAnfitrion($duenio, $request['espacio'], $user));
+
+                    return $user;
                 case 'rejected':
                     return response('El pago fue rechazado. Su tarjeta fue rechazada, intente pagar nuevamente.', 404);
             }
